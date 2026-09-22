a173459f09c4bd2b7b653d8fcbf9c9048a35344f
liwanisaac6-creator <liwanisaac6@gmail.com>
2026-09-22 11:05:41 +0300

    Add files via upload

---
+++ answers.sql
@@ -0,0 +1,18 @@
+-- Question 1
+CREATE TABLE student (
+    id INT PRIMARY KEY,
+    fullName VARCHAR(100),
+    age INT
+);
+
+-- Question 2
+INSERT INTO student (id, fullName, age)
+VALUES
+(1, 'John Kamau', 19),
+(2, 'Mary Wanjiku', 18),
+(3, 'David Otieno', 21);
+
+-- Question 3
+UPDATE student
+SET age = 20
+WHERE id = 2;
